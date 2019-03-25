a39b88be700ec83b37a8587dba2cef176144796d
Pedro Barros <pdbcc.barros@outlook.com>
2019-03-25 19:50:39 -0300

    importando fase incial da agenda

---
+++ agenda.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct pessoa {
+    char nome [20];
+    int idade;
+    int telefone;
+};
+
+int main (){
+    int *pBuffer;
+    int *c;
+    
+    
+}
